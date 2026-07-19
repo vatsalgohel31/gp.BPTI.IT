19e3957942493a23de9953d3cbcdede5d7de67b1
vatsal gohel <153736311+vatsalgohel31@users.noreply.github.com>
2026-07-19 10:45:46 +0530

    Add Python quiz game implementation
    This script implements a Python quiz game with multiple questions and scoring based on user input.

---
+++ sem1/python/GOHEL VATSAL S. 22IT076.py
@@ -0,0 +1,382 @@
+print("modify by :- vatsal  gohel ")
+print("welcome to python quiz game")
+print('''
+game rules :-
+સાચા જવાબ ના  + 10
+ખોટા જવાબ ના    -‌10
+જો જવાબ નથી આવડતો  તો  તેને છોડી  શકો  છો.
+તેના માટે વિકલ્પ 5 છે.
+''')
+
+your = str(input("enter your name :-"))
+
+point = 0
+print(''' questation 1 :-
+python is ______ level  language. ''')
+print(''' 
+1 = high
+2 = low
+3 = both
+4 = none
+''')
+ans1 = int(input(" enter your ans : - "))
+if ans1 is 1 :
+    print("right")
+    point = point + 10
+    print(" your score is : - ",point)
+
+elif ans1 is 2 :
+    print("wrong")
+    point = point - 10
+    print(" your score is : - ",point)
+
+elif ans1 is 3 :
+    print("wrong")
+    point = point - 10
+    print(" your score is : - ",point)
+
+elif ans1 is 4 :
+    print("wrong")
+    point = point - 10
+    print(" your score is : - ",point)
+else :
+    print("skip")
+    point = point - 0
+    print(" your score is : - ",point)
+
+print("--------------------------------------------------------------------------------")
+
+print(''' questation  2 :-
+      Which of  the  following is a step by step solution of  program ? ''')
+print('''
+1 = flowchart
+2 = algorithem
+3 = program
+4 = pseudo code
+''')
+ans2 = int(input(" enter your ans : - "))
+if ans2 is 1 :
+    print("wrong")
+    point = point - 10
+    print(" your score is : - ",point)
+
+elif ans2 is 2 :
+    print("right")
+    point = point + 10
+    print(" your score is : - ",point)
+
+elif ans2 is 3 :
+    print("wrong")
+    point = point - 10
+    print(" your score is : - ",point)
+
+elif ans2 is 4 :
+    print("wrong")
+    point = point - 10
+    print(" your score is : - ",point)
+
+else :
+    print("skip")
+    point = point - 0
+    print(" your score is : - ",point)
+
+print("-----------------------------------------------------------------------")
+
+print(''' questation  3 :-
+      which of the following is a graphical representation of programe ? ''')
+print('''
+1 = flowchart
+2 = algorithem
+3 = program
+4 = pseudo code
+''')
+ans2 = int(input(" enter your ans : - "))
+if ans2 is 1 :
+    print("right")
+    point = point + 10
+    print(" your score is : - ",point)
+
+elif ans2 is 2 :
+    print("wrong")
+    point = point - 10
+    print(" your score is : - ",point)
+
+elif ans2 is 3 :
+    print("wrong")
+    point = point - 10
+    print(" your score is : - ",point)
+
+elif ans2 is 4 :
+    print("wrong")
+    point = point - 10
+    print(" your score is : - ",point)
+
+else :
+    print("skip")
+    point = point - 0
+    print(" your score is : - ",point)
+
+
+print("----------------------------------------------------------------")
+
+print(''' questation  4 :-
+      which symbol is used to connect two parts of flowchart ? ''')
+print('''
+1 = on page connector
+2 = off page connector
+3 = both a  and b
+4 = none
+''')
+ans2 = int(input(" enter your ans : - "))
+if ans2 is 1 :
+    print("wrong")
+    point = point - 10
+    print(" your score is : - ",point)
+
+elif ans2 is 2 :
+    print("wrong")
+    point = point - 10
+    print(" your score is : - ",point)
+
+elif ans2 is 3 :
+    print("right")
+    point = point + 10
+    print(" your score is : - ",point)
+
+elif ans2 is 4 :
+    print("wrong")
+    point = point - 10
+    print(" your score is : - ",point)
+
+else :
+    print("skip")
+    point = point - 0
+    print(" your score is : - ",point)
+
+print("-------------------------------------------------------------------------------------")
+
+print(''' questation  5 :-
+      arithmetic operation can be represented using''')
+print('''
+1 = input/output
+2 = process
+3 = terminal
+4 = Decision
+''')
+ans2 = int(input(" enter your ans : - "))
+if ans2 is 1 :
+    print("wrong")
+    point = point - 10
+    print(" your score is : - ",point)
+
+elif ans2 is 2 :
+    print("right")
+    point = point + 10
+    print(" your score is : - ",point)
+
+elif ans2 is 3 :
+    print("wrong")
+    point = point - 10
+    print(" your score is : - ",point)
+
+elif ans2 is 4 :
+    print("wrong")
+    point = point - 10
+    print(" your score is : - ",point)
+
+else :
+    print("skip")
+    point = point - 0
+    print(" your score is : - ",point)
+
+print("---------------------------------------------------------------------------------")
+
+print(''' questation  6 :-
+      which of the following is the prooerty of algorithem''')
+print('''
+1 = finitness
+2 = effectiveness
+3 = definiteness
+4 = all
+''')
+ans2 = int(input(" enter your ans : - "))
+if ans2 is 1 :
+    print("wrong")
+    point = point - 10
+    print(" your score is : - ",point)
+
+elif ans2 is 2 :
+    print("wrong")
+    point = point - 10
+    print(" your score is : - ",point)
+
+elif ans2 is 3 :
+    print("wrong")
+    point = point - 10
+    print(" your score is : - ",point)
+
+elif ans2 is 4 :
+    print("right")
+    point = point + 10
+    print(" your score is : - ",point)
+
+else :
+    print("skip")
+    point = point - 0
+    print(" your score is : - ",point)
+
+print("------------------------------------------------------------------------------------------------------------------------")
+
+print(''' questation  7 :-
+      who developed the python language ?''')
+print('''
+1 = zim den
+2 = guido van rossum
+3 = niene stom
+4 = wick van rossum
+''')
+ans2 = int(input(" enter your ans : - "))
+if ans2 is 1 :
+    print("wrong")
+    point = point - 10
+    print(" your score is : - ",point)
+
+elif ans2 is 2 :
+    print("right")
+    point = point + 10
+    print(" your score is : - ",point)
+
+elif ans2 is 3 :
+    print("wrong")
+    point = point - 10
+    print(" your score is : - ",point)
+
+elif ans2 is 4 :
+    print("wrong")
+    point = point - 10
+    print(" your score is : - ",point)
+
+else :
+    print("skip")
+    point = point - 0
+    print(" your score is : - ",point)
+
+print("-------------------------------------------------------------------------------------------------------------------------")
+ 
+print(''' questation  8 :-
+      in which year was the python language developed ?''')
+print('''
+1 = 1975
+2 = 1945
+3 = 1991
+4 = 1987
+''')
+ans2 = int(input(" enter your ans : - "))
+if ans2 is 1 :
+    print("wrong")
+    point = point - 10
+    print(" your score is : - ",point)
+
+elif ans2 is 2 :
+    print("wrong")
+    point = point - 10
+    print(" your score is : - ",point)
+
+elif ans2 is 3 :
+    print("right")
+    point = point + 10
+    print(" your score is : - ",point)
+
+elif ans2 is 4 :
+    print("wrong")
+    point = point - 10
+    print(" your score is : - ",point)
+
+else :
+    print("skip")
+    point = point - 0
+    print(" your score is : - ",point)
+
+print("-------------------------------------------------------------------------------------------------------------------------------") 
+
+print(''' questation  9 :-
+      in which language python written ?''')
+print('''
+1 = c
+2 = english
+3 = c++
+4 = java
+''')
+ans2 = int(input(" enter your ans : - "))
+if ans2 is 1 :
+    print("right")
+    point = point + 10
+    print(" your score is :- ",point)
+
+elif ans2 is 2 :
+    print("wrong")
+    point = point - 10
+    print(" your score is :- ",point)
+
+elif ans2 is 3 :
+    print("wrong")
+    point = point - 10
+    print(" your score is :- ",point)
+
+elif ans2 is 4 :
+    print("wrong")
+    point = point - 10
+    print(" your score is :- ",point)
+
+else :
+    print("skip")
+    point = point - 0
+    print(" your score is :- ",point)
+
+print("----------------------------------------------------------------------------------------------------------------------------")
+
+print(''' questation  10 :-
+      what is the exetension of python ? ?''')
+print('''
+1 = .pt
+2 = .p
+3 = .py
+4 = .python
+''')
+ans2 = int(input(" enter your ans : - "))
+if ans2 is 1 :
+    print("wrong")
+    point = point - 10
+    print(" your score is :- ",point)
+
+elif ans2 is 2 :
+    print("wrong")
+    point = point - 10
+    print(" your score is :- ",point)
+
+elif ans2 is 3 :
+    print("right")
+    point = point + 10
+    print(" your score is :- ",point)
+
+elif ans2 is 4 :
+    print("wrong")
+    point = point - 10
+    print(" your score is :- ",point)
+
+else :
+    print("skip")
+    point = point - 0
+    print(" your score is :- ",point)
+
+print("----------------------------------------------------------------------------------------------------------")
+
+print(" your score is :- ",point)
+
+print("total point is 100")
+
+pp = (point/100)*100
+
+print(pp)
+
+print("Thank you so much" ,your ,"you play quiz")
